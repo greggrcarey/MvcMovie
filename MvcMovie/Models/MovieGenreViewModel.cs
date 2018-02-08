1af9df95463225f6cb694a5ca0c1b7bc716cf8c4
Gregg Carey <gcarey@valorem.com>
2018-02-08 09:40:13 -0600

    Added ViewModel

---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public SelectList genres;
+        public List<Movie> movies;
+        public string movieGenre { get; set; }
+    }
+}
